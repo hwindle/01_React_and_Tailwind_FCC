b83903a6ab154f467d8e9ed4c3e2ea03fdd294f1
Hazel Windle <hwindle83@googlemail.com>
2025-09-08 16:08:34 +0100

    First component with props

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Message = (props) => {
+  // get values out of objects.
+  const { text, bgColor } = props;
+  return <h2 style={{ background: bgColor }}>{text}</h2>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Message text="first prop" bgColor="lavender" />
     </div>
   );
 }
