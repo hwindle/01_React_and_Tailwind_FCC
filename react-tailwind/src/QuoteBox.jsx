6a3d495d873150fa9e2c4383f98f2f472e5d559d
Hazel Windle <hwindle83@googlemail.com>
2025-09-11 16:17:02 +0100

    Finished React Tailwind quote component

--- react-tailwind/src/QuoteBox.jsx
+++ react-tailwind/src/QuoteBox.jsx
@@ -2,13 +2,13 @@ const QuoteBox = (props) => {
   const {text, author, bio} = props.quote;
 
   return (
-    <section className="flex flex-col border border-cyan-800">
-      <div className="bg-cyan-800 text-slate-100 p-5 m-4">
+    <section className="flex flex-col m-6">
+      <div className="bg-cyan-800 text-slate-100 p-5 w-[400px] rounded-t-md center text-2xl">
         {text}
       </div>
-      <div className="bg-slate-100 p-5 m-4">
+      <div className="bg-slate-100 p-5 text-lg rounded-bg-md center text-bold">
         {author}
-        <p>{bio}</p>
+        <p className="text-slate-400 text-base">{bio}</p>
       </div>
     </section>
   );
